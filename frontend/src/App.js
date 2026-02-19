5d9f5c28b7586e0df22f1108dfb17fca8a764551
lokesh machavarapu <lokeshm7075@gmail.com>
2026-02-19 12:50:16 +0530

    Initial commit: backend, frontend, k8s, Jenkinsfile

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from Cloud-Native CI/CD Project!</h1>
+      <p>This frontend can communicate with the backend API.</p>
+    </div>
+  );
+}
+
+export default App;
+
